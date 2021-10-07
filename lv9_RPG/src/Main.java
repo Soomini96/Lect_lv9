ebeefa92bfefc54a0e3386e8fddb149645ebb1ce
Soomini96 <91924105+Soomini96@users.noreply.github.com>
2021-10-07 11:08:15 +0900

    .

---
+++ lv9_RPG/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class MainGame{
+	static Scanner scan = new Scanner(System.in);
+	static Random ran = new Random();
+	
+	public MainGame() {
+		
+	}
+	public void run() {
+		while(true) {
+			System.out.println("==== [HOME] ====");
+			System.out.println("[1]길드 관리");
+			System.out.println("[2]상점");
+			System.out.println("[3]인벤토리");
+			System.out.println("----------------");
+			System.out.println("[4]저장");
+			System.out.println("[5]로드");
+			System.out.println("[0]종료");
+			System.out.println("================");
+			System.out.print("메뉴를 입력하세요_ ");
+			String input = scan.next();
+			try {
+				int sel = Integer.parseInt(input);
+				if(sel == 1) {
+					
+				}
+				else if(sel == 2) {
+					
+				}
+				else if(sel == 3) {
+					
+				}
+				else if(sel == 4) {
+					
+				}
+				else if(sel == 5) {
+					
+				}
+				else if(sel == 0) {
+					System.out.println("[게임 종료]");
+					break;
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+	}
+}
+public class Main {
+
+	public static void main(String[] args) {
+		MainGame mg = new MainGame();
+		mg.run();
+	}
+
+}
---
+++ lv9_RPG/src/models/Unit.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Unit {
+	private String name;
+	private int level;
+	private int hp;
+	private int maxHp;
+	private int att; // 공격력
+	private int def; // 방어력
+	private int exp;
+	private boolean party;
+	private Item weapon;
+	private Item armor;
+	private Item ring;
+}
